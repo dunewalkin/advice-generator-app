089949455aab8baa486a7a55963fe6cba40b4fa3
dunewalkin <sergei.gunchenko@gmail.com>
2023-10-16 18:45:48 +0400

    initial commit

--- main.js
+++ main.js
@@ -1,29 +1,49 @@
 const adviceId = document.querySelector('.advice-id');
 const adviceText = document.querySelector('.advice-text');
+const title = document.querySelector('.title');
+const adviceContainer = document.querySelector('.container');
+
 const btn = document.querySelector('.btn');
 
 // Function to fetch advice and update the DOM
 async function getAdvice() {
-   try {
-     const response = await fetch("https://api.adviceslip.com/advice");
-     if (!response.ok) {
-       throw new Error('Failed to fetch advice.');
-     }
-     const adviceData = await response.json();
-     const { id, advice } = adviceData.slip;
- 
-     adviceId.textContent = id;
-     adviceText.textContent = '"' + advice + '"';
-   } catch (error) {
-     console.error(error);
-   } finally {
-      loadingIndicator.style.display = 'none'; // Hide loading indicator
-      btn.disabled = false; // Enable the button after the request
-    }
- }
- 
- // Event listener for the button click
- btn.addEventListener('click', getAdvice);
+  try {
+    adviceContainer.style.pointerEvents = 'none'; // Disable button during transition
+    title.classList.add('hidden');
+    adviceText.classList.add('hidden');
 
- // Initial advice fetch when the page loads
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (!response.ok) {
+      throw Error('Failed to fetch advice.');
+    }
+    const adviceData = await response.json();
+    const { id, advice } = adviceData.slip;
+
+    // Delay to ensure the elements are hidden before updating content
+    setTimeout(() => {
+      // Update the content while still hidden
+      adviceId.textContent = id;
+      adviceText.textContent = '"' + advice + '"';
+
+      // Enable the button and remove "hidden" class after the transition
+      setTimeout(() => {
+        title.classList.remove('hidden');
+        adviceText.classList.remove('hidden');
+
+        // Enable the button and remove the pointer-events block
+        adviceContainer.style.pointerEvents = 'auto';
+
+        // Smoothly transition the container's max-height to fit the new content
+      }, 10); // This should be a very short delay
+
+    }, 500); // This should match the transition duration in CSS (0.5s)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Event listener for the button click
+btn.addEventListener('click', getAdvice);
+
+// Initial advice fetch when the page loads
 window.addEventListener('load', getAdvice);
